773842b1f9187e3511c59e1e699b41d17f7d1a8e
pankajyadav20196 <101635654+pankajyadav20196@users.noreply.github.com>
2022-12-19 00:59:34 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile-iffatbhai"
+    bucket = "euran-terraform-statefile-isra"
     key = "server_name/statefile"
     region = "us-east-1"
   }
